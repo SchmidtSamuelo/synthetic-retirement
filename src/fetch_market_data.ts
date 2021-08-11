759f48048b57ec020789a174d066519c891b46dd
Samuel Schmidt <schmidtsamuelo@gmail.com>
2021-08-11 00:03:30 -0400

    i think i fixed something

--- src/fetch_market_data.ts
+++ src/fetch_market_data.ts
@@ -2,6 +2,7 @@
 import excelToJson from 'convert-excel-to-json';
 import fs from 'fs';
 import axios from 'axios';
+import { OrganizedMarketData } from 'data/data_types';
 import { defaultColumnMap } from './data/column_map';
 import OrganizedCleanedMarketData from './helpers/reorganize_json';
 
@@ -20,20 +21,23 @@ async function fetchData(): Promise<any> {
     url: excelUrl,
     responseType: 'stream',
   })
-    .then((response) => new Promise((resolve) => {
-      response.data.pipe(filestream);
-      let error: boolean = false;
-      filestream.on('error', (err) => {
-        error = Boolean(err);
-        filestream.close();
-        throw err;
-      });
-      filestream.on('close', () => {
-        if (!error) {
-          resolve(true);
-        }
-      });
-    }))
+    .then(
+      (response) =>
+        new Promise((resolve) => {
+          response.data.pipe(filestream);
+          let error: boolean = false;
+          filestream.on('error', (err) => {
+            error = Boolean(err);
+            filestream.close();
+            throw err;
+          });
+          filestream.on('close', () => {
+            if (!error) {
+              resolve(true);
+            }
+          });
+        }),
+    )
     .catch((error) => {
       throw error;
     });
@@ -50,12 +54,19 @@ function buildWriteManipulatedMarketDataJson() {
     sheets: ['Data'], // stock data, other sheets are charts/disclaimers
   });
   // write data to .json file no overwrite
-  const organizedMarketData = new OrganizedCleanedMarketData(rawData.Data).reorganizeJson();
-  fs.writeFileSync(organizedMarketDataPath, JSON.stringify(organizedMarketData), { flag: 'wx' });
+  const organizedMarketData = new OrganizedCleanedMarketData(
+    rawData.Data,
+  ).reorganizeJson();
+
+  fs.writeFileSync(
+    organizedMarketDataPath,
+    JSON.stringify(organizedMarketData),
+    { flag: 'wx' },
+  );
   return organizedMarketData;
 }
 
-async function fetchWriteManipulateMarketData() {
+async function fetchOrWriteManipulatedMarketData(): Promise<OrganizedMarketData> {
   // If we don't have the .json data file we need to build it
   if (!fs.existsSync(organizedMarketDataPath)) {
     // If we also don't have the .xls file fetch it
@@ -66,18 +77,17 @@ async function fetchWriteManipulateMarketData() {
   }
 
   const rawMarketData = fs.readFileSync(organizedMarketDataPath, 'utf-8');
-  const marketData = JSON.parse(rawMarketData);
+  const marketData: OrganizedMarketData = JSON.parse(rawMarketData);
+  console.log(marketData['187102']);
 
   return marketData;
 }
 
-const marketData = fetchWriteManipulateMarketData();
-console.log(marketData);
+async function runScript(): Promise<void> {
+  const marketData = await fetchOrWriteManipulatedMarketData();
+  console.log(marketData['187101']);
+}
 
-// exports.marketData = async (req: any, res: any) => {
-//   const marketData = await fetchWriteManipulateMarketData();
-//   console.log(marketData);
-//   res.json(marketData);
-// };
+runScript();
 
-export { fetchWriteManipulateMarketData as default };
+export default fetchOrWriteManipulatedMarketData;
