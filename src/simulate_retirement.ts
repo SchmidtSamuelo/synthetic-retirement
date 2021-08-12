fb13313de062e7e0af4fe3bc55343c6f04944e86
Samuel Schmidt <schmidtsamuelo@gmail.com>
2021-08-12 13:53:16 -0400

    moved savings simulation to it's own file

--- src/simulate_retirement.ts
+++ src/simulate_retirement.ts
@@ -1,6 +1,5 @@
 import { OrganizedMarketData } from './data/data_types';
-import fetchOrWriteManipulatedMarketData from './fetch_market_data';
-import simulateNMonthSavingsTimeWindow from './helpers/simulate_time_window';
+import { fetchOrWriteManipulatedMarketData } from './fetch_market_data';
 
 async function backtestNPercentRule(
   withdrawPercent: number,
@@ -10,11 +9,3 @@ async function backtestNPercentRule(
   const organizedMarketData: OrganizedMarketData =
     await fetchOrWriteManipulatedMarketData();
 }
-
-// async function backtestMonthlySavingsRate(deposit: number, startingPortfolioBalance: number = 0, simulationLength: number): Promise<void> {
-//   const organizedMarketData: OrganizedMarketData = await fetchOrWriteManipulatedMarketData();
-//   const endingBalances: number[];
-//   for (let i = 0; i < simulationLength; i+=1) {
-
-//   }
-// }
