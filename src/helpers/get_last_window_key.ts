2c424f3a7c7a0001cfc841a683dd3273d1692847
Samuel Schmidt <schmidtsamuelo@gmail.com>
2021-08-16 16:25:40 -0400

    fixed bug with stop conditions

--- src/helpers/get_last_window_key.ts
+++ src/helpers/get_last_window_key.ts
@@ -5,16 +5,15 @@ import { organizedMarketDataPath } from '../fetch_market_data';
 // Get the number of starting points for simulation and last key
 function getSimulationStopConditions(
   timeFrame: number, // months
-): { lastSimulationKey: string; startKeys: string[] } {
+): { lastSimulationKey: string; keys: string[]; numStartKeys: number } {
   const organizedMarketData: OrganizedMarketData = JSON.parse(
     fs.readFileSync(organizedMarketDataPath, 'utf-8'),
   );
 
   const keys = Object.keys(organizedMarketData);
   const numStartKeys = keys.length - timeFrame;
-  const startKeys = keys.slice(0, numStartKeys + 1);
-  console.log(startKeys[startKeys.length - 1]);
-  return { lastSimulationKey: keys[numStartKeys], startKeys }; // Make sure we don't overflow
+  console.log(keys[keys.length - 1]);
+  return { lastSimulationKey: keys[keys.length - 1], keys, numStartKeys };
 }
 
 export default getSimulationStopConditions;
